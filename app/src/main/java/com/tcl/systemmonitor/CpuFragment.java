6ed11dba9ceacd16ccf0e7c7e754f74e42190e92
jianyu.gu <jianyu.gu@tcl.com>
2016-10-09 15:52:10 +0800

    更新

---
+++ app/src/main/java/com/tcl/systemmonitor/CpuFragment.java
@@ -0,0 +1,36 @@
+package com.tcl.systemmonitor;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class CpuFragment extends Fragment {
+
+    private String mParam1;
+    private String mParam2;
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_cpu, container, false);
+    }
+
+
+
+
+
+
+}
